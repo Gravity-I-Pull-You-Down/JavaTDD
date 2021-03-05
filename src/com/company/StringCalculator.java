4f97d5a19b10fc37a9aaa0a8e56f4eefa8087702
Viveksharma48b <viveksharma48b@gmail.com>
2021-03-05 21:37:45 +0530

    3  Add method can handle new lines between numbers

--- src/com/company/StringCalculator.java
+++ src/com/company/StringCalculator.java
@@ -1,12 +1,23 @@
 package com.company;
 
+import org.junit.internal.runners.model.ReflectiveCallable;
+
+import java.util.Vector;
+
 public class StringCalculator {
 
     public static int Add(String number){
-        if(number.length()==0)
+        if(number.trim().length()==0)
             return 0;
         if(!number.contains(","))
             return Integer.parseInt(number);
-        else return Integer.parseInt(String.valueOf(number.charAt(0))) + Integer.parseInt(String.valueOf(number.charAt(2)));
+        else {
+            int index =0,sum =0;
+            while(index<=number.length()){
+                sum += Integer.parseInt(String.valueOf(number.charAt( number.indexOf(',',index)+1)));
+                index+=2;
+            }
+            return sum;
+        }
     }
 }
--- src/com/company/StringCalculatorTest.java
+++ src/com/company/StringCalculatorTest.java
@@ -26,6 +26,27 @@ public class StringCalculatorTest {
         int answer = strclc.Add(str);
         Assert.assertEquals(3,answer);
     }
+    @Test
+    public void AddUnknown() {
+        StringCalculator strclc = new StringCalculator();
+        String str = "5,5,5,5,5";
+        int answer = strclc.Add(str);
+        Assert.assertEquals(25,answer);
+    }
+    @Test
+    public void AddUnknown2() {
+        StringCalculator strclc = new StringCalculator();
+        String str = "5,4,3,2,1";
+        int answer = strclc.Add(str);
+        Assert.assertEquals(15,answer);
+    }
+    @Test
+    public void Addnextline() {
+        StringCalculator strclc = new StringCalculator();
+        String str = "5\n4\n3\n2\n1";
+        int answer = strclc.Add(str);
+        Assert.assertEquals(15,answer);
+    }
 }
 
 //Generated with love by TestMe :) Please report issues and submit feature requests at: http://weirddev.com/forum#!/testme
